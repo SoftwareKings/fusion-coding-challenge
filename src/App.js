af35ff0c07a3166380a9682bd66f7f7db21abd27
SoftwareKings <dragonfighter0120@gmail.com>
2020-04-24 08:39:07 -0700

    Add 2 nav buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [chartType, setChartType] = useState("static");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-navs">
+        <div className="App-navButton" onClick={() => setChartType("static")}>
+          <span>Show Static Graph</span>
+        </div>
+
+        <div className="App-navButton" onClick={() => setChartType("dynamic")}>
+          <span>Show Dyanmic Graph</span>
+        </div>
+      </div>
+
+      <div className="App-charts">
+        <h1>Fusion Chart Graph - {chartType}</h1>
+      </div>
     </div>
   );
 }
